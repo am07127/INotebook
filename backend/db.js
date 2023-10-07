eae73ba4b4ea0ffe9e6e25cbf851e18b0d6aa4d0
am07127 <am07127@st.habib.edu.pk>
2023-10-07 20:13:29 +0500

    backend changes

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
-const mongoURL = 'mongodb://localhost:27017';
+const mongoURL = 'mongodb://localhost:27017/StudentDB';
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURL)
+    mongoose.connect(mongoURL,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=>{
+        console.log("Connected to Mongo Successfully");
+    }).catch((err)=>{
+        console.log("Error connecting to Mongo");
+        console.log(err);
+    })
 }
 
 module.exports = connectToMongo; // exporting the function
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
   res.send('Hello Harry!')
 })
