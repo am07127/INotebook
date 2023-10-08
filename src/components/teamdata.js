23f5ee1d43ed3b4ea2277a50e6e773e60b5732b5
Muhammad Areeb Kazmi <97940168+MuhammadAreebKazmi2@users.noreply.github.com>
2023-10-08 09:02:19 +0500

    Update teamdata.js

--- src/components/teamdata.js
+++ src/components/teamdata.js
@@ -1,5 +1,5 @@
 
 
-const TeamData = [{name: "Wajeeha Farooqi",image:"/PICTURES/WAJEEHA 1.jpg", title:"Project Manager",id:1}, {name:"Imaara Zulfiqar Ali",image:"/PICTURES/IMAARA.jpg", title:"Research Analyst", id:2}, {name:"Alishba Karam",image:"/PICTURES/ALISHBA.jpeg", title:"Social Media Manager", id:3}]
+const TeamData = [{name: "Wajeeha Farooqi",image:"/PICTURES/WAJEEHA 1.jpg", title:"Project Manager", info:"Researcher, Project Coordinator, and Marketing Coordinator",id:1}, {name:"Imaara Zulfiqar Ali",image:"/PICTURES/IMAARA.jpg", title:"Research Analyst",info:"Habib University graduate, BSc (Hons) in Social Development and Policy" ,id:2}, {name:"Alishba Karam",image:"/PICTURES/ALISHBA.jpeg", title:"Social Media Manager", info:"Computer Science Student, Graphic Designer, Content Creator", id:3}]
 
 export default TeamData;
