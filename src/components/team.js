3901607aa8f829d98b000dd86d03545f8be432a5
Muhammad Areeb Kazmi <97940168+MuhammadAreebKazmi2@users.noreply.github.com>
2023-10-08 08:32:17 +0500

    Update team.js - Team sections added

--- src/components/team.js
+++ src/components/team.js
@@ -1,146 +1,102 @@
 import React, { Component } from "react";
 import TeamData from "./teamdata";
+import Advisor from "./advisordata";
 
 export default class team extends Component {
   render() {
     return (
-      <div>
-        <div className="flex items-center justify-center text-center">
-          <div className="flex flex-col items-center justify-center rounded-tr-full rounded-bl-full w-full bg-white">
-            <div className="flex flex-col p-2 m-2 max-w-7xl">
-              <div className="text-xl md:text-3xl font-medium py-8">
-                Meet Our Team
-              </div>
+<section class="text-gray-600 body-font bg-blue-300">
+  <div class="container px-5 py-24 mx-auto">
+  <div class="flex flex-col text-center w-full mb-20">
+    <h1 class="text-4xl font-arial font-bold title-font mb-4 text-gray-700 tracking-wider">Meet Our Team</h1>
+    <h2 class="lg:w-2/3 mx-auto leading-relaxed text-xl text-gray-900 font-semibold">Management Team</h2>
+  </div>
 
-              <div className="flex items-center justify-center lg:flex-row flex-col space-x-4 space-y-4 p-2">
-                {TeamData.map((person) => {
-                  return (
-                    <div
-                      className="flex-col px-6 py-2 w-[90%] md:w-[50%] lg:w-1/4"
-                      key={person.id}
-                    >
-                      <div className="h-40 w-40 border-2 border-yellow-400 rounded-full overflow-hidden bg-gray-200">
-                        <img src={person.image} alt="" className="" />
-                      </div>
-
-                      <div className="text-md text-white cursor-pointer w-[50%] bg-yellow-400 lg:rotate-90 mr-40 mt-8 lg:mb-16 pr-4 pl-1 py-1">
-                        Designer
-                      </div>
-                      <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400">
-                        {person.name}
-                      </div>
-                      <div className="text-left m-2">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Exercitationem deserunt, dolorum illo consequatur, natus
-                        aut, esse dignissimos rem facilis ipsa numquam
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
+    <div class="flex flex-wrap -m-4 justify-center">
+    {TeamData.map((person) => {
+      return (
+        <div class="p-4 lg:w-1/2" key = {person.id}>
+          <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={person.image} />
+            <div class="flex-grow sm:pl-8">
+              <h2 class="title-font font-bold text-lg text-gray-900">{person.name}</h2>
+              <h3 class="text-gray-900 font-medium mb-3">{person.title}</h3>
+              <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+              <span class="inline-flex">
+                <a class="text-gray-500">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-2 text-gray-500">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-2 text-gray-500">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                </a>
+              </span>
             </div>
           </div>
         </div>
-        <script src="https://cdn.tailwindcss.com"></script>
-        <script src="https://use.fontawesome.com/03f8a0ebd4.js"></script>
-        <script
-          type="module"
-          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
-        ></script>
-        <script
-          nomodule
-          src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
-        ></script>
-        <div className="text-xl md:text-3xl font-medium py-8">
-          Our Mission Statement
+          );
+          })}
         </div>
-        <div className="flex justify-center">
-          <div className="flex flex-col max-w-7xl justify-center items-center">
-            <div className="overflow-hidden w-3/4 bg-white m-4 shadow-lg flex flex-col md:flex-row justify-center">
-              <div className="h-26 w-full overflow-hidden">
-                {" "}
-                <img
-                  src="https://source.unsplash.com/random/500x400/?knowledge "
-                  alt=""
-                  className=""
-                />{" "}
-              </div>
-              <div className="grid p-2">
-                <div className="font-bold text-lg text-black m-2 mt-10">
-                  Building Knowledge and Skills{" "}
-                </div>
-                <div className="text-gray-500 m-2 text-sm">
-                  <a href="">
-                    Our team will collaborate with knowledge and skill based
-                    individuals and organizations around a diversified basket of
-                    knowledge and skill sets for training people belonging to
-                    less privileged, low income households and communities. Aim
-                    is to empower them for better living through gainful income
-                    generation – benefits we feel are not just financial, but
-                    also social and societal through improved access to quality
-                    education and healthcare and avoidance of engaging in
-                    non-productive, unhealthy life pursuits.
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="overflow-hidden w-3/4 bg-white m-4 shadow-lg flex flex-col md:flex-row justify-center">
-              <div className="grid p-2">
-                <div className="font-bold text-lg text-black m-2 mt-10">
-                  Access to Employment and Entrepreneurship{" "}
-                </div>
-                <div className="text-gray-500 m-2 text-sm">
-                  <a href="">
-                    Knowledg enhancement and skills development would follow by
-                    assisting communities in accessing gainful employment or on
-                    treading the path of entrepreneurship. Here again, our team
-                    of Mera Hunar Mera Kaam would collaborate with individuals
-                    and organizations that could act as employers or as
-                    facilitators in setting up businesses. <br></br>Financial
-                    Support – When it comes to setting up of businesses – we
-                    would provide financial support after careful assessment and
-                    validation of the business plan.{" "}
-                  </a>
-                </div>
-              </div>
-              <div className="h-26 w-full overflow-hidden">
-                {" "}
-                <img
-                  src="https://source.unsplash.com/random/500x400/?entrepreneurship "
-                  alt=""
-                  className=""
-                />{" "}
-              </div>
-            </div>
-            <div className="overflow-hidden w-3/4 bg-white m-4 shadow-lg flex flex-col md:flex-row justify-center">
-              <div className="h-26 w-full overflow-hidden">
-                {" "}
-                <img
-                  src="https://source.unsplash.com/random/500x400/?books "
-                  alt=""
-                  className=""
-                />{" "}
-              </div>
-              <div className="grid p-2">
-                <div className="font-bold text-lg text-black m-2 mt-10">
-                  Supporting Social Causes{" "}
-                </div>
-                <div className="text-gray-500 m-2 text-sm">
-                  <a href="">
-                    Another area of our work would involve supporting existing
-                    social causes where we have made a beginning by setting up a
-                    <strong> Book Bank </strong>– collecting books from people
-                    willing to donate, or buying from low budget vendors and
-                    then distributing them in schools run for low income
-                    communities.{" "}
-                  </a>
-                </div>
-              </div>
+        {/* Second Section */}
+      <div class="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center justify-center w-full mb-8 mt-12">
+          <h1 className="text-4xl font-arial font-bold title-font mb-4 text-gray-700 tracking-wider"></h1>
+          <h2 className="lg:w-2/3 mx-auto leading-relaxed text-xl text-gray-900 font-semibold">Advisory Panel</h2>
+        </div>
+
+        <div className="flex flex-wrap -m-4 justify-center mt-8">
+          {/* Add content for the second section here */}
+        
+        {Advisor.map((person1) => {
+      return (
+        <div class="p-4 lg:w-1/2" key = {person1.id}>
+          <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src={person1.image} />
+            <div class="flex-grow sm:pl-8">
+              <h2 class="title-font font-bold text-lg text-gray-900">{person1.name}</h2>
+              <h3 class="text-gray-900 font-medium mb-3">{person1.title}</h3>
+              <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+              <span class="inline-flex">
+                <a class="text-gray-500">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-2 text-gray-500">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                  </svg>
+                </a>
+                <a class="ml-2 text-gray-500">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                </a>
+              </span>
             </div>
           </div>
         </div>
-        <script src="https://cdn.tailwindcss.com"></script>
+          );
+          })}
+        </div>
+        {/* End of Second Section */}
+          </div>
       </div>
-    );
-  }
+</section>
+
+
+
+);
 }
+}
+
+
+
